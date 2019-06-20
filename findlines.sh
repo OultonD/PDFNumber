9d4409d599477788dc7ba9d7d16217653ae56574
Daniel Oulton <daniel.oulton@gmail.com>
2019-06-20 11:34:45 -0400

    init

---
+++ findlines.sh
@@ -0,0 +1,9 @@
+rm -rf $1-*.txt
+convert $1.pdf png:$1
+for f in $1-*; do 
+convert $f -flatten -strip -resize 1X1000!\
+ -black-threshold 99% -white-threshold 10% -negate -morphology Erode Diamond\
+ -define morphology:compose=Darken\
+ -morphology Thinning:-1 '1x3:0,1,1' -black-threshold 50% txt:-\
+| sed -e '1d' -e '/#000000/d' -e 's/^[^,]*,//' -e 's/[(]//g' -e 's/:.*//' -e 's/,/ /g' > $f.txt;
+done
